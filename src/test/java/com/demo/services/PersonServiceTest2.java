e50207950729843346f57f2da20acf8204f3b267
Satyendra patel <Satyendrapatel652@gmail>
2022-08-06 14:17:19 +0530

    First commit

---
+++ src/test/java/com/demo/services/PersonServiceTest2.java
@@ -0,0 +1,14 @@
+package com.demo.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonServiceTest2 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
